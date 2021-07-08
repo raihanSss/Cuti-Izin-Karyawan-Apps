6b47978428e5c2baf9a82d97b523e33a58c968af
fahrul_ihsan <email@example.com>
2021-07-08 20:34:41 +0700

    layouting template

---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+@include('includes.header')
+<body>
+  <div id="app">
+    <div class="main-wrapper main-wrapper-1">
+        @include('includes.navbar')
+
+        @include('includes.sidebar')
+      
+        @yield('content')
+
+        @include('includes.footer')
+
+    </div>
+  </div>
+  
+  @yield('script')
+  @include('includes.script')
+</body>
+
+
+<!-- Mirrored from radixtouch.com/templates/admin/aegis/source/light/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 07 Jul 2021 09:55:44 GMT -->
+</html>
